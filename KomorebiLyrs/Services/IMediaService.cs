2c2586ee01663ec7977ebc63b0fbb4d5d10379e7
結露 <68598198+XketsuroX@users.noreply.github.com>
2025-12-27 18:46:36 +0800

    Init project

---
+++ KomorebiLyrs/Services/IMediaService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace KomorebiLyrs.Services;
+
+public interface IMediaService
+{
+    event EventHandler<string> SongChanged;
+    Task InitializeAsync();
+}
---
+++ KomorebiLyrs/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using Avalonia.Threading;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace KomorebiLyrs.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    
+
+    [ObservableProperty] private string mainLyrics = "Waiting for music";
+    
+    private readonly DispatcherTimer _timer;
+    private int _count;
+    public MainWindowViewModel()
+    {
+        _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(3) };
+        _timer.Tick += Timer_tick;
+        
+        _timer.Start();
+    }
+
+    private void Timer_tick(object? sender, EventArgs e)
+    {
+        _count++;
+
+        MainLyrics = _count % 2 == 0 ? "誰かの願いが叶うころ" : "あの子が泣いてるよ";
+    }
+
+    
+}
